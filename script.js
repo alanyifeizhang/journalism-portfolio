1ef02fa8074bf726268a3c99ed9a594a5453a8d6
Alan Zhang <azhang318@student.fuhsd.org>
2026-05-24 19:11:43 -0700

    Publish journalism portfolio

---
+++ script.js
@@ -0,0 +1,292 @@
+const portfolioData = {
+  articles: [
+    {
+      title: "New members in court",
+      href: "https://issuu.com/thehhsepitaph/docs/issue_1_689891e0cbea1b",
+      image: "https://static.wixstatic.com/media/ef1e20_cc8059403c934e7d9e25526677d7e393~mv2.jpg/v1/fill/w_446,h_297,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_cc8059403c934e7d9e25526677d7e393~mv2.jpg",
+    },
+    {
+      title: "Online jokes take dark humor to new lows",
+      href: "https://hhsepitaph.com/15214/arts-culture/jokes-intensifies-disgusting-humor-online/",
+      image: "https://static.wixstatic.com/media/ef1e20_1cded162e28c45a19476d5589787653b~mv2.png/v1/fill/w_391,h_391,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_1cded162e28c45a19476d5589787653b~mv2.png",
+    },
+    {
+      title: "Shift in role causes leadership changes",
+      href: "https://issuu.com/thehhsepitaph/docs/issue_1_689891e0cbea1b",
+      image: "https://static.wixstatic.com/media/ef1e20_861a0cbf9aed468b8f780bb6461007aa~mv2.png/v1/fill/w_293,h_391,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_861a0cbf9aed468b8f780bb6461007aa~mv2.png",
+    },
+    {
+      title: "More than just a resource",
+      href: "https://issuu.com/thehhsepitaph/docs/issue_2_f26135a40978de",
+      image: "https://static.wixstatic.com/media/ef1e20_d22e97dab2f5428d821725490767e3ac~mv2.jpg/v1/fill/w_446,h_297,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_d22e97dab2f5428d821725490767e3ac~mv2.jpg",
+    },
+    {
+      title: "Science olympiad triggers members' personal development",
+      href: "https://hhsepitaph.com/15244/lifestyles/science-olympiad-triggers-members-personal-development/",
+      image: "https://static.wixstatic.com/media/ef1e20_7a16cc9c464d40a5bfb17baa7528f4e9~mv2.jpg/v1/fill/w_446,h_297,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_7a16cc9c464d40a5bfb17baa7528f4e9~mv2.jpg",
+    },
+    {
+      title: "HHS must speak up for public speaking curriculum",
+      href: "https://hhsepitaph.com/15280/opinion/hhs-must-speak-up-for-public-speaking-curriculum/",
+      image: "https://static.wixstatic.com/media/ef1e20_9dee693d4c2941129af1812d18a5d186~mv2.png/v1/fill/w_490,h_490,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_9dee693d4c2941129af1812d18a5d186~mv2.png",
+    },
+    {
+      title: "Robotics team rolls toward upcoming...",
+      href: "https://issuu.com/thehhsepitaph/docs/issue_4_1ccde917a6a620",
+      image: "https://static.wixstatic.com/media/ef1e20_31303eb1b3ff48a2abe7a09810ec59ae~mv2.png/v1/fill/w_446,h_333,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_31303eb1b3ff48a2abe7a09810ec59ae~mv2.png",
+    },
+    {
+      title: "Chinese department hosts first C-Night",
+      href: "https://issuu.com/thehhsepitaph/docs/issue_4_1ccde917a6a620",
+      image: "https://static.wixstatic.com/media/ef1e20_be3a3d096e1e430fa60f9091c4b5b3e5~mv2.jpg/v1/fill/w_446,h_297,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_be3a3d096e1e430fa60f9091c4b5b3e5~mv2.jpg",
+    },
+    {
+      title: "Trustees begin term following new...",
+      href: "https://issuu.com/thehhsepitaph/docs/issue_4_1ccde917a6a620",
+      image: "https://static.wixstatic.com/media/ef1e20_1fd8f2ebbe1d4dedb4794f6678187529~mv2.jpg/v1/fill/w_446,h_297,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_1fd8f2ebbe1d4dedb4794f6678187529~mv2.jpg",
+    },
+    {
+      title: "Meet the counseling team",
+      href: "https://issuu.com/thehhsepitaph/docs/the_epitaph_volume_62_issue_5_2024-25",
+      image: "https://static.wixstatic.com/media/ef1e20_4685f2a9d5a7401898526186a29aa66a~mv2.jpg/v1/fill/w_439,h_329,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_4685f2a9d5a7401898526186a29aa66a~mv2.jpg",
+    },
+    {
+      title: "FBLA sends record numbers to SLC",
+      href: "https://issuu.com/thehhsepitaph/docs/homestead_high_school_s_student_newspaper_the_epit",
+      image: "https://static.wixstatic.com/media/ef1e20_8fbcdfd3b6cc4f2281945ab2c5985ac0~mv2.jpg/v1/fill/w_446,h_297,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_8fbcdfd3b6cc4f2281945ab2c5985ac0~mv2.jpg",
+    },
+    {
+      title: "Student-led nonprofit introduces...",
+      href: "https://issuu.com/thehhsepitaph/docs/homestead_high_school_s_student_newspaper_the_epit",
+      image: "https://static.wixstatic.com/media/ef1e20_0954aa71d4af47c7b0e05504b9a4aa83~mv2.jpeg/v1/fill/w_417,h_313,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_0954aa71d4af47c7b0e05504b9a4aa83~mv2.jpeg",
+    },
+    {
+      title: "WASC concludes six-year accreditation...",
+      href: "https://issuu.com/thehhsepitaph/docs/the_epitaph_volume_62_issue_6_2024-25",
+      image: "https://static.wixstatic.com/media/ef1e20_601a26e73d7346ae9cfa3e3fb8ca492a~mv2.jpg/v1/fill/w_446,h_295,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_601a26e73d7346ae9cfa3e3fb8ca492a~mv2.jpg",
+    },
+    {
+      title: "Long-time law teacher set to retire",
+      href: "https://issuu.com/thehhsepitaph/docs/homestead_high_school_s_student_newspaper_the_epit",
+      image: "https://static.wixstatic.com/media/ef1e20_3c9422cb8a934da2b609478e630595f8~mv2.jpg/v1/fill/w_446,h_297,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_3c9422cb8a934da2b609478e630595f8~mv2.jpg",
+    },
+    {
+      title: "Frausto's last dance",
+      href: "https://issuu.com/thehhsepitaph/docs/the_epitaph_volume_62_issue_8_2024-25",
+      image: "https://static.wixstatic.com/media/ef1e20_c03ffd8bc0604440ae0c96de1bc7ca8a~mv2.jpg/v1/fill/w_239,h_319,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_c03ffd8bc0604440ae0c96de1bc7ca8a~mv2.jpg",
+    },
+    {
+      title: "Traditional dance allows cultural identity to",
+      href: "https://issuu.com/thehhsepitaph/docs/the_epitaph_volume_63_issue_1_2025-26",
+      image: "https://static.wixstatic.com/media/ef1e20_2fba65c5b0d74defb8c07b6a3d6407cc~mv2.jpg/v1/fill/w_429,h_286,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_2fba65c5b0d74defb8c07b6a3d6407cc~mv2.jpg",
+    },
+    {
+      title: "ASB changes improve class culture",
+      href: "https://issuu.com/thehhsepitaph/docs/the_epitaph_volume_63_issue_1_2025-26",
+      image: "https://static.wixstatic.com/media/ef1e20_7de805a9aecb4c2fbc7dd4e372c08b0f~mv2.jpg/v1/fill/w_400,h_267,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_7de805a9aecb4c2fbc7dd4e372c08b0f~mv2.jpg",
+    },
+    {
+      title: "TSA to bring back Taiwanese...",
+      href: "https://issuu.com/thehhsepitaph/docs/the_epitaph_volume_63_issue_2_2025-26",
+      image: "https://static.wixstatic.com/media/ef1e20_53a8da19562c4cd58104ab8c5a06be02~mv2.jpg/v1/fill/w_400,h_267,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_53a8da19562c4cd58104ab8c5a06be02~mv2.jpg",
+    },
+    {
+      title: "Brainrot has rotted",
+      href: "https://issuu.com/thehhsepitaph/docs/the_epitaph_volume_63_issue_2_2025-26",
+      image: "https://static.wixstatic.com/media/ef1e20_f62e7d993e1e406eb8af88a4cb94d7e3~mv2.png/v1/fill/w_306,h_408,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_f62e7d993e1e406eb8af88a4cb94d7e3~mv2.png",
+    },
+    {
+      title: "JV field hockey scores big with...",
+      href: "https://hhsepitaph.com/15706/sports/jv-field-hockey-scores-big-with-large-roster/",
+      image: "https://static.wixstatic.com/media/ef1e20_078ec48fdc214d3ba81e28b2e4e95626~mv2.png/v1/fill/w_400,h_266,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_078ec48fdc214d3ba81e28b2e4e95626~mv2.png",
+    },
+    {
+      title: "FNHS build linguistic bridges...",
+      href: "https://issuu.com/thehhsepitaph/docs/the_epitaph_volume_63_issue_3_2025-26",
+      image: "https://static.wixstatic.com/media/ef1e20_cdb04100a0a34b6e85198a063cb0dec2~mv2.jpeg/v1/fill/w_373,h_280,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_cdb04100a0a34b6e85198a063cb0dec2~mv2.jpeg",
+    },
+  ],
+  multimedia: [
+    {
+      title: "Robotics building process",
+      href: "https://issuu.com/thehhsepitaph/docs/issue_4_1ccde917a6a620",
+      image: "https://static.wixstatic.com/media/ef1e20_b54bba3c7f604f01bb88b43bd714679e~mv2.png/v1/fill/w_421,h_110,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_b54bba3c7f604f01bb88b43bd714679e~mv2.png",
+    },
+    {
+      title: "Superbowl reel",
+      href: "https://www.instagram.com/reel/DF3Uvv6z2jN/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_6372f3d83cf5476290d1ce58d6f0f29d~mv2.png/v1/fill/w_189,h_337,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_6372f3d83cf5476290d1ce58d6f0f29d~mv2.png",
+    },
+  ],
+  social: [
+    {
+      title: "Senior Sunrise",
+      href: "https://www.instagram.com/p/C_CbEDzJEbX/?img_index=1",
+      image: "https://static.wixstatic.com/media/ef1e20_48994dcca3a14babaf8714b59b608553~mv2.jpg/v1/fill/w_421,h_281,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_48994dcca3a14babaf8714b59b608553~mv2.jpg",
+    },
+    {
+      title: "CSF Blood Drive",
+      href: "https://www.instagram.com/p/DB0GDC9t616/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_712ba59af62f4c4f9a036819bad2c19c~mv2.jpg/v1/fill/w_421,h_281,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_712ba59af62f4c4f9a036819bad2c19c~mv2.jpg",
+    },
+    {
+      title: "Blacklight Rally",
+      href: "https://www.instagram.com/p/DDVS_nuyJkm/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_1263f28f574f495c9e57eb0c9b5bca2f~mv2.jpg/v1/fill/w_421,h_281,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_1263f28f574f495c9e57eb0c9b5bca2f~mv2.jpg",
+    },
+    {
+      title: "Girls Golf",
+      href: "https://www.instagram.com/p/DBfLXhavMxZ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_23424ba7a70a4fd7ba8eab7ecc00dd45~mv2.jpg/v1/fill/w_421,h_281,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_23424ba7a70a4fd7ba8eab7ecc00dd45~mv2.jpg",
+    },
+    {
+      title: "SCV MUN",
+      href: "https://www.instagram.com/p/DFlthc9yQb9/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_42e578f1ad634141a3b55bb922dbad07~mv2.jpg/v1/fill/w_421,h_279,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_42e578f1ad634141a3b55bb922dbad07~mv2.jpg",
+    },
+    {
+      title: "Boys Soccer Senior Night",
+      href: "https://www.instagram.com/p/DGHVjW-y-l6/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_b429834552034a74a8f80bbfa2d390a1~mv2.png/v1/fill/w_337,h_276,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_b429834552034a74a8f80bbfa2d390a1~mv2.png",
+    },
+    {
+      title: "Boys Baseball",
+      href: "https://www.instagram.com/p/DHhGsiQyL7s/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_668e40c9c7e6480d81e3ab4c4fcb5d4f~mv2.png/v1/fill/w_421,h_284,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_668e40c9c7e6480d81e3ab4c4fcb5d4f~mv2.png",
+    },
+    {
+      title: "Band Banquet",
+      href: "https://www.instagram.com/p/DJkzwWhvO0o/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_da3e7e2226de463ca3214bc73462b2b4~mv2.jpg/v1/fill/w_421,h_281,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_da3e7e2226de463ca3214bc73462b2b4~mv2.jpg",
+    },
+    {
+      title: "Prom",
+      href: "https://www.instagram.com/p/DJ715SYSsXc/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_cb57bf3f75f8484fa0a48ab19b326c39~mv2.jpg/v1/fill/w_421,h_281,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_cb57bf3f75f8484fa0a48ab19b326c39~mv2.jpg",
+    },
+    {
+      title: "Spring Jazz Concert",
+      href: "https://www.instagram.com/p/DKYC1Y5ywmq/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_8c8735d915bb493cb9c0997288dd7c95~mv2.jpg/v1/fill/w_421,h_279,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_8c8735d915bb493cb9c0997288dd7c95~mv2.jpg",
+    },
+    {
+      title: "Garba",
+      href: "https://www.instagram.com/p/DPUP_q9DxXX/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_e9356daaaee4444d8d9983ba5fb2815b~mv2.png/v1/fill/w_396,h_299,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_e9356daaaee4444d8d9983ba5fb2815b~mv2.png",
+    },
+    {
+      title: "Field Hockey",
+      href: "https://www.instagram.com/p/DO6acYYjxD9/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_7ceb288893034f0491856b9c11e8bb95~mv2.png/v1/fill/w_376,h_286,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_7ceb288893034f0491856b9c11e8bb95~mv2.png",
+    },
+    {
+      title: "Back to School Night",
+      href: "https://www.instagram.com/p/DOkZZCSjuEJ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_596eb4a22f434a558eb9022bf6e59c28~mv2.png/v1/fill/w_421,h_277,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_596eb4a22f434a558eb9022bf6e59c28~mv2.png",
+    },
+    {
+      title: "HOCO Week Day 2",
+      href: "https://www.instagram.com/p/DOkZZCSjuEJ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_7376bf6b88ef46679597f756db17871f~mv2.png/v1/fill/w_421,h_300,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_7376bf6b88ef46679597f756db17871f~mv2.png",
+    },
+    {
+      title: "ASB Thrift Store",
+      href: "https://www.instagram.com/p/DSToIK-ifKp/?utm_source=ig_web_copy_link&igsh=NTc4MTIwNjQ2YQ==",
+      image: "https://static.wixstatic.com/media/ef1e20_c98f23a741954863ad7645e295236c0e~mv2.png/v1/fill/w_421,h_286,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_c98f23a741954863ad7645e295236c0e~mv2.png",
+    },
+    {
+      title: "Club Fair",
+      href: "https://www.instagram.com/p/DQ0iX_8kbNT/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+      image: "https://static.wixstatic.com/media/ef1e20_1a611c2cf82b439f881c385e51e30a23~mv2.png/v1/fill/w_421,h_286,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/ef1e20_1a611c2cf82b439f881c385e51e30a23~mv2.png",
+    },
+  ],
+};
+
+const labelByType = {
+  articles: "Article",
+  multimedia: "Multimedia",
+  social: "Social Media",
+};
+
+function sourceLabel(url) {
+  if (url.includes("instagram.com")) return "Instagram";
+  if (url.includes("issuu.com")) return "Issuu";
+  if (url.includes("drive.google.com")) return "Google Drive";
+  if (url.includes("filesusr.com")) return "PDF";
+  return "The Epitaph";
+}
+
+function createPortfolioCard(item, type) {
+  const card = document.createElement("article");
+  card.className = "portfolio-card";
+
+  const image = document.createElement("img");
+  image.src = item.image;
+  image.alt = item.title;
+  image.loading = "lazy";
+
+  const body = document.createElement("div");
+  body.className = "portfolio-card-body";
+
+  const meta = document.createElement("div");
+  meta.className = "portfolio-meta";
+
+  const format = document.createElement("span");
+  format.textContent = labelByType[type];
+
+  const source = document.createElement("span");
+  source.textContent = sourceLabel(item.href);
+
+  const title = document.createElement("h3");
+  title.textContent = item.title;
+
+  const description = document.createElement("p");
+  description.textContent =
+    type === "social" ? "Social media coverage." : type === "multimedia" ? "Multimedia reporting." : "Published work.";
+
+  const link = document.createElement("a");
+  link.href = item.href;
+  link.target = "_blank";
+  link.rel = "noreferrer";
+  link.textContent = type === "social" ? "Open post" : "Open work";
+  link.setAttribute("aria-label", `Open ${item.title}`);
+
+  meta.append(format, source);
+  body.append(meta, title, description, link);
+  card.append(image, body);
+  return card;
+}
+
+function renderPortfolio() {
+  Object.entries(portfolioData).forEach(([type, items]) => {
+    const panel = document.querySelector(`[data-panel="${type}"]`);
+    const grid = document.createElement("div");
+    grid.className = "portfolio-grid";
+
+    items.forEach((item) => {
+      grid.append(createPortfolioCard(item, type));
+    });
+
+    panel.replaceChildren(grid);
+  });
+}
+
+function activateTab(type) {
+  document.querySelectorAll(".portfolio-tab").forEach((tab) => {
+    const isActive = tab.dataset.tab === type;
+    tab.classList.toggle("active", isActive);
+    tab.setAttribute("aria-selected", String(isActive));
+  });
+
+  document.querySelectorAll(".portfolio-panel").forEach((panel) => {
+    const isActive = panel.dataset.panel === type;
+    panel.classList.toggle("active", isActive);
+    panel.hidden = !isActive;
+  });
+}
+
+document.querySelectorAll(".portfolio-tab").forEach((tab) => {
+  tab.addEventListener("click", () => activateTab(tab.dataset.tab));
+});
+
+renderPortfolio();
